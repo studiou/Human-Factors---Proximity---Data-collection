43d51ccc55fb879421bbcff4bb3b54236988060c
studiou.ge@gmail.com <studiou.ge@gmail.com>
2017-12-21 15:37:57 -0500

    Update

--- main.py
+++ main.py
@@ -28,27 +28,35 @@ imported_xls_data = {}
 
 #get data from excel spreadsheet and dump it into imported_xls_data array. Just pass the file path to the function and it returns an array with all data from the array
 imported_xls_data = Read_xlsx_Scoresheet.importfromxls(allxlsxfilepaths)
-#imported_xls_data[x][0] = sex of participant (i.e. male or female
+#imported_xls_data[x][0] = gender of participant (i.e. male or female
+gender = 0
 #imported_xls_data[x][1] = age of participant (i.e.adult or child)
+age = 1
 #imported_xls_data[x][2] = event (i.e. hit or miss)
+event = 2
 #imported_xls_data[x][3] = the target (i.e. Dispense, dispensor Controls, Fridge door (dispensor side), Fridge door (no dispensor), Freezer Door)
+target =3
 
 #imported_xls_data[x][4] = foot distance of the 1st step onto the MAT with LEFT foot (in inches)
+Y_1st_LEFT_step = 4
 #imported_xls_data[x][5] = angle of the 1st step onto the MAT with LEFT foot (in degrees)
+Angle_1st_LEFT_step = 5
 #imported_xls_data[x][6] = timestamp of the 1st step onto the MAT with LEFT foot (in 00:00:000 hours, mins, milliseconds)
+Timestamp_1st_LEFT_step = 6
 
 #imported_xls_data[x][7] = foot distance of the 1st  step onto the MAT with RIGHT foot (in inches)
-
+Y_1st_RIGHT_step = 7
 #imported_xls_data[x][8] = angle of the 1st  step onto the MAT with RIGHT foot (in degrees)
-
+Angle_1st_RIGHT_step = 8
 #imported_xls_data[x][9] = timestamp of the 1st  step onto the MAT with RIGHT foot (in 00:00:000 hours, mins, milliseconds)
+Timestamp_1st_RIGHT_step = 9
 
 #imported_xls_data[x][10] = foot distance of the LEFT foot at fridge (if a hit) (in inches)
-Y_at_fridge_LEFT_step = 4
+Y_at_fridge_LEFT_step = 10
 #imported_xls_data[x][11] = angle of the LEFT foot at fridge (if a hit) (in degrees)
-Angle_at_fridge_LEFT_step = 5
+Angle_at_fridge_LEFT_step = 11
 #imported_xls_data[x][12] = timestamp of the LEFT foot at fridge (if a hit) (in 00:00:000 hours, mins, milliseconds)
-Timestamp_at_fridge_LEFT_step = 6
+Timestamp_at_fridge_LEFT_step = 12
 
 #imported_xls_data[x][13] = foot distance of the RIGHT foot at fridge (if a hit) (in inches)
 #imported_xls_data[x][14] = angle of the RIGHT foot at fridge (if a hit) (in degrees)
