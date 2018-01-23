b252fd76dc094b33a607d67926f2f14db014f927
joel <joelpersinger@icloud.com>
2018-01-23 08:31:22 -0500

    test

---
+++ Processed_Data_Class.py
@@ -0,0 +1,13 @@
+__author__ = 'joel'
+class P_Data(object):
+
+    def __init__(self, sex):
+        self.sex = sex
+    def__init__(self, age):
+        self.age = age
+    def__init__(self, event):
+        self.event = event #hit or miss
+    def__init__(self, target):
+        self.target = target #if hit what is the target dispensor, fridge door, freezer
+
+self.group = groupClass(1, 2, 3)
