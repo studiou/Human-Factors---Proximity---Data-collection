cc0384c9bddd82344b6dd64783f28c06127cbc7f
steven_root <Steven.Root@ge.com>
2018-02-06 17:23:51 -0500

    updated distance function and tests

--- DistanceFromPointToLine.py
+++ DistanceFromPointToLine.py
@@ -7,15 +7,18 @@ def DistanceFromPointToLine(line_x1,line_y1, line_x2,line_y2, point_x, point_y):
     # handle all but a vertical line
     if (line_x1 != line_x2):
         slope = (line_y2 - line_y1)/(line_x2 - line_x1)
-        intercept = line_y1 - (slope * line_x1)
         a = -1.0 * slope
         b = 1.0
-        c = -1 * intercept
-        print "\n got here! slope = " + str(slope) + " intercept = " + str(intercept) 
-
-        distance = abs((a * point_x) + (b * point_y) + c)/((a**2 + b**2) ** 0.5)
-        xOnLine = ((b * ((b*line_x1) - (b*line_y1))) - (a * c))/(a**2 + b**2)
-        yOnLine = ((a * ((-1.0 * b * line_x1) + (a * line_y1))) - (b * c))/(a**2 + b**2)
+        if (slope != 0):
+            intercept = line_y1 - (slope * line_x1)
+            c = -1 * intercept
+            distance = round(abs((a * point_x) + (b * point_y) + c)/((a**2 + b**2) ** 0.5),2)
+            xOnLine = round(((b * ((b*point_x) - (b*point_y))) - (a * c))/(a**2 + b**2),2)
+            yOnLine = round(((a * ((-1.0 * b * point_x) + (a * point_y))) - (b * c))/(a**2 + b**2),2)
+        else:
+            distance = abs(line_y1 - point_y)
+            xOnLine = point_x
+            yOnLine = line_y1
     else:   # line is vertical
         distance = abs(line_x1 - point_x)
         xOnLine = line_x1
--- DistanceFromPointToLine_Test.py
+++ DistanceFromPointToLine_Test.py
@@ -13,6 +13,16 @@ y1_45DegreeLine = 20.0
 x2_45DegreeLine = 20.0
 y2_45DegreeLine = 0.0
 
+x1_VerticalLine = 50.0
+y1_VerticalLine = 0.
+x2_VerticalLine = 50.0
+y2_VerticalLine = 1000.
+
+x1_HorizontalLine = 50.0
+y1_HorizontalLine = 1000.
+x2_HorizontalLine = 500.0
+y2_HorizontalLine = 1000.
+
 class TestDistanceFromPointToLine(unittest.TestCase):
     """ Unit Test Framework Class """
     def setUp(self):
@@ -23,34 +33,58 @@ class TestDistanceFromPointToLine(unittest.TestCase):
 
     def testPointBelow45DegreeLine(self):
         distance, x, y = DistanceFromPointToLine(x1_45DegreeLine, y1_45DegreeLine, x2_45DegreeLine, y2_45DegreeLine, 0, 0)
-        print distance, x, y 
-        self.assertEqual(20.0, distance)
-        self.assertEqual(0.0, x)
-        self.assertEqual(20.0, y)
-
+        self.assertEqual(14.14, distance)
+        self.assertEqual(10.0, x)
+        self.assertEqual(10.0, y)
+ 
     def testPointAbove45DegreeLine(self):
-        pass
-            
+        # extending the 45 degree line 14.14 * 2!  should yield 20,20 we have made a square
+        distance, x, y = DistanceFromPointToLine(x1_45DegreeLine, y1_45DegreeLine, x2_45DegreeLine, y2_45DegreeLine, 20.0, 20.0)
+        self.assertEqual(14.14, distance)
+        self.assertEqual(10.0, x)
+        self.assertEqual(10.0, y)        
+             
     def testPointOn45DegreeLine(self):
-        pass
-    
+        distance, x, y = DistanceFromPointToLine(x1_45DegreeLine, y1_45DegreeLine, x2_45DegreeLine, y2_45DegreeLine, 10.0, 10.0)
+        self.assertEqual(0, distance)
+        self.assertEqual(10.0, x)
+        self.assertEqual(10.0, y)        
+     
     def testPointLeftOfLineWithInfinteSlope(self):
-        pass
-    
+        distance, x, y = DistanceFromPointToLine(x1_VerticalLine, y1_VerticalLine, x2_VerticalLine, y2_VerticalLine, 10.0, 10.0)
+        self.assertEqual(40.0, distance)
+        self.assertEqual(x2_VerticalLine, x)
+        self.assertEqual(10.0, y)       
+             
     def testPointRightOfLineWithInfinteSlope(self):
-        pass
-    
+        distance, x, y = DistanceFromPointToLine(x1_VerticalLine, y1_VerticalLine, x2_VerticalLine, y2_VerticalLine, 550.0, 10.0)
+        self.assertEqual(500.0, distance)
+        self.assertEqual(x2_VerticalLine, x)
+        self.assertEqual(10.0, y)
+             
     def testPointOnLineWithInfinteSlope(self):
-        pass
+        distance, x, y = DistanceFromPointToLine(x1_VerticalLine, y1_VerticalLine, x2_VerticalLine, y2_VerticalLine, 50.0, 10.0)
+        self.assertEqual(0.0, distance)
+        self.assertEqual(x2_VerticalLine, x)
+        self.assertEqual(10.0, y)
     
     def testPointAboveLineWith0Slope(self):
-        pass
+        distance, x, y = DistanceFromPointToLine(x1_HorizontalLine, y1_HorizontalLine, x2_HorizontalLine, y2_HorizontalLine, 50.0, y2_HorizontalLine+10.0)
+        self.assertEqual(10.0, distance)
+        self.assertEqual(50, x)
+        self.assertEqual(y2_HorizontalLine, y)
 
     def testPointBelowLineWith0Slope(self):
-        pass
-
+        distance, x, y = DistanceFromPointToLine(x1_HorizontalLine, y1_HorizontalLine, x2_HorizontalLine, y2_HorizontalLine, 50.0, y2_HorizontalLine-10.0)
+        self.assertEqual(10.0, distance)
+        self.assertEqual(50, x)
+        self.assertEqual(y2_HorizontalLine, y)
+        
     def testPointOnLineWith0Slope(self):
-        pass
+        distance, x, y = DistanceFromPointToLine(x1_HorizontalLine, y1_HorizontalLine, x2_HorizontalLine, y2_HorizontalLine, 50.0, y2_HorizontalLine)
+        self.assertEqual(0.0, distance)
+        self.assertEqual(50, x)
+        self.assertEqual(y2_HorizontalLine, y)        
         
 if __name__ == "__main__":
     unittest.main()
