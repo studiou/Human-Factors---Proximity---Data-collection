8dbb7163c8f9b37527c7fd0f06a2d9a000610a97
joel <joelpersinger@icloud.com>
2018-05-07 15:56:46 -0400

    V2: Create Bootstrap function to test if vector crosses area of detection

--- Bootstrap.py
+++ Bootstrap.py
@@ -6,54 +6,37 @@ __author__ = 'joel'
 
 import math
 
-# Check whether vector coordinates lie on the same side of the triangle
-def side(vecX1, vecY1, vecX2, vecY2, sideX1, sideY1, sideX2,  sideY2):
-    z1 = float ((sideX2 - sideX1) * (vecY1 - sideY1) - (vecX1 - sideX1) * (sideY2 - sideY1))
-    z2 = float ((sideX2 - sideX1) * (vecY2 - sideY1) - (vecX2 - sideX1) * (sideY2 - sideY1))
-    return z1 * z2
+# Check to see if two lines intersect
+# return a 1 if they intersect
+# return a 0 if the do not intersect
+def line_intersection_test( p0_x, p0_y, p1_x, p1_y, p2_x, p2_y, p3_x, p3_y):
 
 
+    s1_x = float(p1_x - p0_x)
+    s1_y = float(p1_y - p0_y)
+    s2_x = float(p3_x - p2_x)
+    s2_y = float(p3_y - p2_y)
+    outcome = 0
 
-def intersecting(vecX1, vecY1, vecX2, vecY2, triX1, triY1, triX2, triY2,triX3, triY3):
-     #Check whether segment is outside one of the three half-planes
-     #delimited by the triangle.
-    #vector 1
-    output = "ERROR"
-    #vector 1
-    f1 = float(side(vecX1, vecY1, triX3, triY3, triX1, triY1, triX2, triY2))
-    f3 = float(side(vecX1, vecY1, triX1, triY1, triX2, triY2, triX3, triY3))
-    f5 = float(side(vecX1, vecY1, triX2, triY2, triX3, triY3, triX1, triY1))
-    #vector 2
-    f2 = float(side(vecX2, vecY2, triX3, triY3, triX1, triY1, triX2, triY2))
-    f4 = float(side(vecX2, vecY2, triX1, triY1, triX2, triY2, triX3, triY3))
-    f6 = float(side(vecX2, vecY2, triX2, triY2, triX3, triY3, triX1, triY1))
+    s = float (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y)
+    t = float ( s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y)
 
-    #Check whether triangle is totally inside one of the two half-planes
-    #delimited by the segment.
-    f7 = side(triX1, triY1, triX2, triY2, vecX1, vecY1, vecX2, vecY2)
-    f8 = side(triX2, triY2, triX3, triY3, vecX1, vecY1, vecX2, vecY2)
+    if (s >= 0 and s <= 1 and t >= 0 and t <= 1):
+        outcome = 1
 
-    #Otherwise we're intersecting with at least one edge */
-    output = "motion detected"
+    return outcome
 
+def triangle_intersection_test(vecX1, vecY1, vecX2, vecY2, triX1, triY1, triX2, triY2,triX3, triY3):
 
-    #If segment is strictly outside triangle, or triangle is strictly
-    #apart from the line, we're not intersecting
-    if ((f1 < 0 and f2 < 0) or (f3 < 0 and f4 < 0) or (f5 < 0 and f6 < 0) or (f7 > 0 and f8 > 0)):
-        output = "motion not detected"
+    side_1_test = line_intersection_test(vecX1, vecY1, vecX2, vecY2, triX1, triY1, triX2, triY2)
+    side_2_test = line_intersection_test(vecX1, vecY1, vecX2, vecY2, triX2, triY2, triX3, triY3)
+    side_3_test = line_intersection_test(vecX1, vecY1, vecX2, vecY2, triX1, triY1, triX3, triY3)
 
-    #If segment is aligned with one of the edges, we're overlapping */
-    if ((f1 == 0 and f2 == 0) or (f3 == 0 and f4 == 0) or (f5 == 0 and f6 == 0)):
-        output = "motion detected"
+    result = side_1_test + side_2_test + side_3_test
 
-    #If segment is outside but not strictly, or triangle is apart but
-    #not strictly, we're touching */
-    if ((f1 <= 0 and f2 <= 0) or (f3 <= 0 and f4 <= 0) or (f5 <= 0 and f6 <= 0) or (f7 >= 0 and f8 >= 0)):
-        output = "motion detected"
+    if result > 0:
+        outcome = "motion detected"
+    else:
+        outcome = "motion not detected"
 
-    #/* If both segment points are strictly inside the triangle, we
-    # * are not intersecting either */
-    if (f1 > 0 and f2 > 0 and f3 > 0 and f4 > 0 and f5 > 0 and f6 > 0):
-        output = "motion not detected"
-
-    return output
+    return outcome
--- Bootstrap_Test.py
+++ Bootstrap_Test.py
@@ -11,16 +11,29 @@ triX3 = 4
 triY3 = 2
 
 #intersecting vector
-IN_vecX1 = 2
-IN_vecY1 = 3
-IN_vecX2 = 4
+IN_vecX1 = 1
+IN_vecY1 = 5
+IN_vecX2 = 3
 IN_vecY2 = 1
 
 #non-intersecting vector
 Non_IN_vecX1 = 1
-Non_IN_vecY1 = 2
+Non_IN_vecY1 = 1
 Non_IN_vecX2 = 1
 Non_IN_vecY2 = 5
+
+# boarding vector
+#Vector 1
+BVec1p1_x = 1
+BVec1p1_y = 5
+BVec1p2_x = 4
+BVec1p2_y = 4
+
+#Vector 2
+BVec2p1_x = 3
+BVec2p1_y = 3
+BVec2p2_x = 5
+BVec2p2_y = 5
 
 
 
@@ -34,14 +47,21 @@ class Speedcal(unittest.TestCase):
         pass
 
     def test_intersecting_test1(self):
-        output = Bootstrap.intersecting(IN_vecX1, IN_vecY1,IN_vecX2,IN_vecY2, triX3, triY3, triX1, triY1, triX2, triY2)
+        output = Bootstrap.triangle_intersection_test(IN_vecX1, IN_vecY1,IN_vecX2,IN_vecY2, triX1, triY1, triX2, triY2, triX3, triY3)
         expectedTimeOutcome = "motion detected"
         self.assertEqual(expectedTimeOutcome, output)
 
     def test_intersecting_test2(self):
-        output = Bootstrap.intersecting(Non_IN_vecX1, Non_IN_vecY1,Non_IN_vecX2,Non_IN_vecY2, triX3, triY3, triX1, triY1, triX2, triY2)
+        output = Bootstrap.triangle_intersection_test(Non_IN_vecX1, Non_IN_vecY1,Non_IN_vecX2,Non_IN_vecY2, triX1, triY1, triX2, triY2, triX3, triY3)
         expectedTimeOutcome = "motion not detected"
         self.assertEqual(expectedTimeOutcome, output)
+
+    def test_intersecting_test5(self):
+        output = Bootstrap.line_intersection_test(BVec1p1_x, BVec1p1_y, BVec1p2_x, BVec1p2_y, BVec2p1_x, BVec2p1_y, BVec2p2_x, BVec2p2_y)
+        expectedTimeOutcome = 1
+        self.assertEqual(expectedTimeOutcome, output)
+
+
 
 if __name__ == "__main__":
     unittest.main()
